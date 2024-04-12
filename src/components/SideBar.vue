6644ba4815c2e40513f9ea48052e1f93a9e04f62
jonmatthis <jonmatthis@gmail.com>
2024-04-12 17:53:21 -0400

    animated icons

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -11,11 +11,11 @@ import SideBarIcon from "@/components/SideBarIcon.vue";
 <template>
   <div
       class="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-gray-300 text-2xl justify-center items-center shadow-2xl">
-    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'fire']" size="2x" /></SideBarIcon>
-    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'plus']" size="2x" /></SideBarIcon>
-    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'bolt-lightning']" size="2x" /></SideBarIcon>
-    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'gear']" size="2x" /></SideBarIcon>
-    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'poo']" size="2x" /></SideBarIcon>
+    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'fire']" size="lg" /></SideBarIcon>
+    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'plus']" size="lg" /></SideBarIcon>
+    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'bolt-lightning']" size="lg" /></SideBarIcon>
+    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'gear']" size="lg" /></SideBarIcon>
+    <SideBarIcon><FontAwesomeIcon :icon="['fas', 'poo']" size="lg" /></SideBarIcon>
   </div>
 </template>
 
--- src/components/SideBarIcon.vue
+++ src/components/SideBarIcon.vue
@@ -8,11 +8,7 @@
 // You can define props and composition functions here if needed
 </script>
 
-<style scoped>
-.sidebar-icon {
-  /* Your CSS styles for sidebar-icon */
-}
-</style>
+
 
 
 
